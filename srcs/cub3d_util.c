03db6c8a7dd5d5754a65825f23ebf6e04b87840b
Yongjae Lee <yongjale@c3r1s2.42seoul.kr>
2023-11-01 13:28:21 +0900

    parse and merge

--- srcs/cub3d_util.c
+++ srcs/cub3d_util.c
@@ -6,25 +6,11 @@
 /*   By: yongjale <yongjale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 13:00:43 by yongjale          #+#    #+#             */
-/*   Updated: 2023/11/01 13:02:53 by yongjale         ###   ########.fr       */
+/*   Updated: 2023/11/01 13:25:22 by yongjale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d_info.h"
-
-void	my_mlx_pixel_put(t_data *image, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = image->addr + (y * image->line_length + x * (image->bpp / 8));
-	*(unsigned int *)dst = color;
-}
-
-int	finish_cub3d(t_box *tools)
-{
-	free(tools);
-	exit(0);
-}
 
 int	sp(char c)
 {
